86f122f8e7ce212cdb024d6340697542867e5d21
Zebra <maciekzgk@gmail.com>
2024-03-24 22:10:38 +0100

    more universal way of connecting with module via rfcomm

--- connect.sh
+++ connect.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/bash
 
 #actual mac address of HC-05 module: 58:56:00:01:06:36
-MAC_HC="58:56:00:01:06:36"
+#MAC_HC="58:56:00:01:06:36"
+#MAC_HC="5C:F6:DC:40:C6:DD"
+#MAC_HC="20:50:E7:E6:63:83"
+MAC_HC="60:35:73:86:64:6E"
+#check for MAC addresses connected to ports of rfcomm
+MAC_CON=$(rfcomm -a | grep "$MAC_HC")
+rfcomm_port=0
 
-#sprawdź czy port rfcomm0 jest utworzony
-if [ ! -e "/dev/rfcomm0" ]; then
+
+
+if [ -n "$MAC_CON" ]; then
+	 echo "HC-05 module already connected to: $MAC_CON"
+else 
 	scan_output=$(hcitool scan)
 	if echo "$scan_output" | grep -q "$MAC_HC"; then
-		sudo rfcomm bind 0 "$MAC_HC"
-		sudo chmod 660 "/dev/rfcomm0"
-		echo "HC-05 module connected"
+		while [ "$rfcomm_port" -le 29 ] 
+		do
+			if [ ! -e "/dev/rfcomm$rfcomm_port" ]; then
+				if sudo rfcomm bind "$rfcomm_port" "$MAC_HC"; then
+					sudo chmod a+rw "/dev/rfcomm$rfcomm_port"
+					echo "HC-05 module connected to a port /dev/rfcomm$rfcomm_port"
+					exit
+				else
+					echo "error while trying to connect to /dev/rfcomm$rfcomm_port"
+					exit
+				fi
+			fi
+			((rfcomm_port++))
+		done
 	else
-		echo "There is no such device in range"	
-		exit
+		echo "There is no HC-05 module in range"	
 	fi
-else
-	echo "port rfcomm0 already in use"
 fi
-
-
+		
